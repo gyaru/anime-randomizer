22a929c9de94bbd900637bfffc5f72718d2f7ae6
ギャル <gyaru@users.noreply.github.com>
2018-10-24 23:43:24 +0200

    wait i'm dumb

--- src/api.ts
+++ src/api.ts
@@ -33,30 +33,13 @@ query ($season: MediaSeason!, $seasonYear: Int!, $page: Int) {
 `;
 
 const seasons = ["WINTER", "SPRING", "SUMMER", "FALL"];
-const years = [
-  "2005",
-  "2006",
-  "2007",
-  "2008",
-  "2009",
-  "2010",
-  "2011",
-  "2012",
-  "2013",
-  "2014",
-  "2015",
-  "2016",
-  "2017",
-  "2018",
-];
+
 export let getRandomAnime = asyncMiddleware(
   async (req: Request, res: Response) => {
     const randomSeason = await seasons[
       Math.floor(Math.random() * Math.floor(seasons.length))
     ];
-    const randomYear = await years[
-      Math.floor(Math.random() * Math.floor(years.length))
-    ];
+    const randomYear = await Math.floor(Math.random() * 12) + 2006;
     const variables = await {
       season: randomSeason,
       seasonYear: randomYear,
